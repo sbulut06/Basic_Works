4d02139c92c3218c20cd94f01f48c7a9054d08c0
Selçuk Bulut <87428595+sbulut06@users.noreply.github.com>
2021-08-01 02:22:27 +0300

    Create body_mass_index.py

---
+++ body_mass_index.py
@@ -0,0 +1,22 @@
+Heigh = float(input("Your Weight: "))
+Weight = float(input("Your Height: "))
+Result = Heigh / (Weight/100)**2
+
+print("Your BMI: {}".format(round(Result)))
+print("Your BMI Categories: ")
+
+
+if Result <= 18.5:
+    print("Underweight")
+elif Result <= 24.9:
+    print("Normal weight")
+elif Result <= 29.9:
+    print("Overweight")
+elif Result <= 39.9:
+    print(" low-risk Obesity")
+elif Result <= 34.9:
+    print("Obese (Class 1) ")
+elif Result <= 39.9:
+    print("Obese (Class 2) ")   
+else:
+    print("Obese (Class 3) ")
